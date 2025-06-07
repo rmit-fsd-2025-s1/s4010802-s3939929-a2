db61c59969a9fff5e196ffa1c893c577ee10b0ca
Nihar <s4010802@student.rmit.edu.au>
2025-06-07 09:23:52 +0530

    added env file and changed the URL

--- frontend/src/services/tutorServices.ts
+++ frontend/src/services/tutorServices.ts
@@ -1,6 +1,6 @@
 import { Application } from "../types/Application";
 
-const API_BASE_URL = "http://localhost:3004/api";
+const API_BASE_URL =  process.env.NEXT_PUBLIC_API_URL;
 
 export async function saveTutorApplication(application: Application) {
   try {
